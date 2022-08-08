813f94f4d529ee971f1737df2f065496acc84029
Dorothy Muhonja <dorothymuhonja7@gmail.com>
2022-08-08 13:09:05 +0300

    Tweepy project

---
+++ twitter_bot.py
@@ -0,0 +1,63 @@
+import tweepy
+
+print('Tweepy Rocks!!!')
+
+CONSUMER_KEY = 'BgUagDjg64h8kxdXq8t2M5DFA'
+CONSUMER_SECRET = 'bY99cvGfcrrkvFmFIuQgESGoYzO9SQ1iAs4AeQw8Kee2F6fQjX'
+ACCESS_KEY = '752093014160465920-5jew3zhIOtkJ2KBCDqgoTtjdSHAzcm3'
+ACCESS_SECRET = 'OIrAvdKjQu3c9rj344pNl1Oc4HqH2LTjdUuuKDscMH6IY'
+
+#authenticate to twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+#creating API object
+api = tweepy.API(auth)
+
+
+#method for mentions
+# mentions = api.mentions_timeline()
+
+# for mention in mentions:
+#     print(f"{str(mention.id)}: {mention.text}")
+#     if '#helloworld' in mention.text.lower():
+#         print('found #helloworld')
+#         print('responding back...')
+#         api.update_status(f"{'@'} {mention.user.screen_name} #Helloworld back to you.")
+
+
+#method for user timelines
+# timeline = api.home_timeline()
+
+# for tweet in timeline:
+#     print(f"{tweet.user.name} said {tweet.text}")
+
+# api.update_status("Test tweet from Tweepy Python")
+
+user = api.get_user(screen_name="dorothy_muhonja")
+print('User details:')
+print(user.name)
+print(user.description)
+# print(user.location)
+
+
+# method for followers
+print('last 20 followers:')
+for follower in user.followers():
+    print(follower.name)
+
+
+api.create_friendship(screen_name='realpython')
+
+# updating profile
+api.update_profile(description='Just your everyday human')
+
+#method for likes(marking most recent tweet as liked)
+# tweets = api.home_timeline(count=1)
+# tweet = tweets[0]
+# print(f"Liked tweet {tweet.id} of {tweet.author.name}")
+# api.create_favorite(tweet.id)
+
+# method for seeing blocked users
+for block in api.blocks():
+    print(block.name)
